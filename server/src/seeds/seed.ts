89fb4f37369faa468d59193b8bf9c2b7ca361ccf
apoborka <alex.poborka@gmail.com>
2025-04-27 20:21:07 -0500

    Updated require() to import() on seed.ts

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,13 +2,17 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-const pythonQuestions = require('./pythonQuestions.json');
-
 db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+  try {
+    const pythonQuestions = (await import('./pythonQuestions.json', { assert: { type: "json" } })).default;
 
-  await Question.insertMany(pythonQuestions);
+    await cleanDB('Question', 'questions');
+    await Question.insertMany(pythonQuestions);
 
-  console.log('Questions seeded!');
-  process.exit(0);
+    console.log('Questions seeded!');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error seeding the database:', err);
+    process.exit(1);
+  }
 });
