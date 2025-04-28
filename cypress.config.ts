a62fc1fdf1edf7b64f36cb1e53688c4cd958e440
apoborka <alex.poborka@gmail.com>
2025-04-27 22:20:47 -0500

    Updated README and PR merge test access to server

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,9 +12,6 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3001',
   },
 });
